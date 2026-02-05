fb224e19fe1b7587de14853d5c82e9cb4a9f5ba5
HeyItsHeart <townerlucas@gmail.com>
2026-02-04 18:29:28 -0600

    Update db.js
    fixed path errors

--- app/db.js
+++ app/db.js
@@ -1,6 +1,19 @@
 import sqlite3 from "sqlite3";
+import path from "path";
+import { fileURLToPath } from "url";
 
-export const db = new sqlite3.Database("./data/accesspoint.db");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const dbPath = path.join(__dirname, "data", "accesspoint.db");
+
+export const db = new sqlite3.Database(dbPath, err => {
+  if (err) {
+    console.error("Failed to connect to SQLite DB:", err);
+  } else {
+    console.log("Connected to SQLite DB");
+  }
+});
 
 db.serialize(() => {
   db.run(`
@@ -14,3 +27,4 @@ db.serialize(() => {
     )
   `);
 });
+
